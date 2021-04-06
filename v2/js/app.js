ce43d97aebb0577236df0e5d09df3eb6c2c8cb11
Sotnas <mr.sansan777@gmail.com>
2021-04-06 18:05:11 -0400

    Trabajando con los eventos jQuery addClaas y removeClass juento con toggleClass

---
+++ v2/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    let parrafo = $('#resultado p');
+
+    $('.btn-primary').click(function () {
+
+        parrafo.addClass('display-4')
+    })
+
+    $('.btn-danger').click(function(){
+        parrafo.removeClass('display-4')
+    })
+
+    $('.btn-warning').click(function (){
+        parrafo.toggleClass('display-4')
+    })
+})
